b4bd950a39b5224be58d45886b70715d4dac60a8
mohinesh9999 <mohineshsharma9999@gmail.com>
2020-11-25 12:39:23 +0530

    deployable added admin login

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn hack_backend.wsgi
+web: python my_django_app/manage.py collectstatic --noinput; bin/gunicorn_django --workers=4 --bind=0.0.0.0:$PORT hack_backend/settings.py 
--- hack_backend/urls.py
+++ hack_backend/urls.py
@@ -1,6 +1,8 @@
 from rest_framework_simplejwt import views as jwt_views 
 from django.urls import path,include
 from django.contrib import admin
+from django.conf.urls import *
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', 
@@ -10,3 +12,7 @@ urlpatterns = [
          jwt_views.TokenRefreshView.as_view(), 
          name ='token_refresh'), 
 ]
+urlpatterns += staticfiles_urlpatterns()
+# urlpatterns += patterns('',
+#  (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+#  )
